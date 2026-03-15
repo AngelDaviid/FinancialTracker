1754a3f5429a101654221360b5626292e41614e8
Angel David <angel_dvalenciae@soy.sena.edu.co>
2026-03-15 14:56:46 -0500

    API|(implementation): first migration added in the proyect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { HealthResolver } from './health/health.resolver';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'prisma/schema.graphql'),
+      sortSchema: true,
+      context: ({ req }) => ({ req }),
+      buildSchemaOptions: {
+        numberScalarMode: 'integer',
+      },
+      playground: true,
+    }),
+  ],
+  providers: [HealthResolver],
 })
 export class AppModule {}
