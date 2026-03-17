941a59d531c4678cebf7ad54551e98f10dc596c0
Angel David <angel_dvalenciae@soy.sena.edu.co>
2026-03-17 16:55:48 -0500

    Revert "Feat/create categories module"

--- src/category/commands/crate-category/create-category.command.ts
+++
@@ -1,8 +0,0 @@
-import { CreateCategoryInput } from '../../dto/create-category.input';
-
-export class CreateCategoryCommand {
-  constructor(
-    public readonly userId: string,
-    public readonly input: CreateCategoryInput,
-  ) {}
-}
--- src/category/commands/update-category/update-category.command.ts
+++
@@ -1,9 +0,0 @@
-import { UpdateCategoryInput } from '../../dto/update-category.input';
-
-export class UpdateCategoryCommand {
-  constructor(
-    public readonly id: string,
-    public readonly userId: string,
-    public readonly input: UpdateCategoryInput,
-  ) {}
-}
