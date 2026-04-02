2fcd557af7dc4bef05d17c087a1d2cb34d6437a7
Angel David <angel_dvalenciae@soy.sena.edu.co>
2026-04-02 11:01:09 -0500

    feat(graphql): update code generation configuration and dependencies

--- FinanceFrontend/codegen.ts
+++ FinanceFrontend/codegen.ts
@@ -1,13 +1,26 @@
-import { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'http://localhost:3000/graphql',
+  overwrite: true,
+  schema: {
+    'http://localhost:3000/graphql': {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-apollo-operation-name': 'IntrospectionQuery',
+      },
+    },
+  },
   documents: ['src/graphql/**/*.graphql'],
   generates: {
-    './src/generated/': {
-      preset: 'client',
+    './src/generated/graphql.ts': {
+      plugins: [
+        '@graphql-codegen/typescript',
+        '@graphql-codegen/typescript-operations',
+        '@graphql-codegen/typescript-react-apollo',
+      ],
       config: {
         useTypeImports: true,
+        apolloReactHooksImportFrom: '@apollo/client',
       },
     },
   },
